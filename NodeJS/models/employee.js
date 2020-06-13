6184d50c73237c4869056923886e2a5440fe72bb
Morsheda <morshedajeba@gmail.com>
2020-06-13 18:50:40 +0600

    initial commit

---
+++ NodeJS/models/employee.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+const mongoose = require('mongoose');
+
+var Employee = mongoose.model('Employee', {
+    name: { type: String },
+    position: { type: String },
+    office: { type: String },
+    salary: { type: Number }
+});
+
+module.exports = { Employee };
